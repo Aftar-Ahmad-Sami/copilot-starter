9ec1b27fbcdd16327b77ba0d2357b366ba1e74eb
Aftar Ahmad Sami <aftarahmadsami@gmail.com>
2024-05-15 10:42:09 +0600

    initial update

--- app.py
+++ app.py
@@ -3,11 +3,33 @@ import time
 
 app = Flask(__name__)
 
+def hello():
+    return "Hi, Guys!"
+
 def seconds_since_epoch():
     epoch = time.time()
-    return int(epoch)
+    return str(epoch)
+
+def days_since_epoch():
+    epoch = time.time()
+    days = epoch / 86400
+    return str(days)
 
 @app.route("/")
 def seconds():
-    return seconds_since_epoch()
+    return hello()
+
+@app.route("/hours")
+def hours():
+    epoch = time.time()
+    hours = epoch / 3600
+    return str(hours)
+
+@app.route("/days")
+def days():
+    return days_since_epoch()
+
+@app.route("/date")
+def date():
+    return time.ctime()
 
